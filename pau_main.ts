d4d20f4312b7a9d92706d44fb9beb4d690ae6fd0
Oriol Rocabert <oriol.rocabert.valverde@gmail.com>
2023-04-20 20:11:51 +0200

    Getting all needed information from api

---
+++ pau_main.ts
@@ -0,0 +1,7 @@
+import { writeFile } from "fs/promises";
+import { render } from "./render.ts";
+import { loadUsers } from "./users.ts";
+
+const users = await loadUsers(100);
+const html = render(users);
+await writeFile("users.html", html);
